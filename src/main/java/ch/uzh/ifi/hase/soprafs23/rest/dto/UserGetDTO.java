a8255e6808ec819ba4b0d27a5d59ceaac2c2e13e
lindasteiner2 <linda.steiner2@uzh.ch>
2023-03-27 10:37:00 +0200

    deleted some stuff

--- src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/UserGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/rest/dto/UserGetDTO.java
@@ -3,7 +3,6 @@ package ch.uzh.ifi.hase.soprafs23.rest.dto;
 import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
 
 public class UserGetDTO {
-
   private Long id;
   private String name;
   private String username;
