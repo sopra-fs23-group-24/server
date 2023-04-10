94808da3e36b9486f99c1cd92250fdced9273d59
lindasteiner2 <linda.steiner2@uzh.ch>
2023-04-10 16:32:10 +0200

    prompt types

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/constant/PromptType.java
@@ -0,0 +1,27 @@
+package ch.uzh.ifi.hase.soprafs23.constant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+public enum PromptType {
+
+    TEXT("TEXT", "1"),
+    TRUEFALSE("TRUEFALSE", "2"),
+    DRAWING("DRAWING", "3");
+
+    private final String statusAsString;
+    private final String statusAsStringNr;
+    PromptType(String statusAsString, String statusAsStringNr){
+        this.statusAsString = statusAsString;
+        this.statusAsStringNr = statusAsStringNr;
+    }
+
+    public static PromptType transformToStatus(String wantedStatus){
+        for(PromptType s : PromptType.values()){
+            if(wantedStatus.equals(s.statusAsString) || wantedStatus.equals(s.statusAsStringNr)){
+                return s;
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid prompt type.");
+    }
+}
